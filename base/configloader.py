ca8927ed250f2f4de0d4d2304d4cfb7d37b8fc23
ntfuko77 <ringochichidim@gmail.com>
2025-04-03 18:37:23 +0800

    simple

--- base/configloader.py
+++ base/configloader.py
@@ -3,6 +3,6 @@ import json
 class Configloader:
     def __init__(self):...
     def load_botkey(self)->dict:
-        with open('discord-battle-game/profile/botkey.json') as file:
+        with open('discord-battle-game/profile/botkey.json',encoding='utf-8') as file:
             botkey_data=json.load(file)
             return botkey_data
--- base/discord_bot_base.py
+++ base/discord_bot_base.py
@@ -4,18 +4,17 @@ import discord
 from discord.ext import commands
 
 class BotBase():
-    def load_token(self) -> str:
-        bottoken = configloader.Configloader().load_botkey()['key']
-        return bottoken
+    def __init__(self):
+        self.config = configloader.Configloader().load_botkey()
+        self.bot = None
     def set_Intents(self) -> discord.Intents:
         intents = discord.Intents.default()
         intents.message_content = True
         return intents
     def login(self) -> None:
-        token = self.load_token()
         self.bot=discord.Client(intents=self.set_Intents())
-        print(' has connected to Discord!')
-        self.bot.run(token)
+        print(f'{self.config["bot_name"]} has connected to Discord!')
+        self.bot.run(self.config['key'])
 
 if __name__ == "__main__":
     bot = BotBase()
